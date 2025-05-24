143b4e9f1488f7777a3b4641f0ebbddf1796162e
rerakamui <dokkakumaru@gmail.com>
2025-05-25 03:24:57 +0900

    fix: correct document path

--- rag_logic.py
+++ rag_logic.py
@@ -5,9 +5,9 @@ from langchain.document_loaders import TextLoader
 loader = TextLoader("docs_sample.txt")
 documents = loader.load()
 
-embeddings = OpenAIEmbeddings()
-db = FAISS.from_documents(documents, embeddings)
 
+db = FAISS.from_documents(documents, embeddings)
+loader = TextLoader("docs_sample.txt")
 def search_documents(query):
     docs = db.similarity_search(query)
     return [{"content": d.page_content} for d in docs]
